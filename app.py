ce20346edf6137274af1f64ba1e57bdbd5adbb4b
majidali5-devops <khajamajidali5@gmail.com>
2026-07-07 17:08:06 -0400

    Update app.py

--- app.py
+++ app.py
@@ -2,7 +2,7 @@ from http.server import HTTPServer, BaseHTTPRequestHandler
 
 class MyHandler(BaseHTTPRequestHandler):
     def do_GET(self):
-        self.send_response(200)
+        self.send_response(6000)
         self.send_header("Content-type", "text/plain")
         self.end_headers()
         self.wfile.write(b"Hello from Python server!")
