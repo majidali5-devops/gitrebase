39b1f199bd62cbb5189ae964a85e0855fd9562eb
majidkhaja69 <majidkhaja69@gmail.com>
2026-07-07 17:07:26 -0400

    added new code

---
+++ app.py
@@ -0,0 +1,16 @@
+from http.server import HTTPServer, BaseHTTPRequestHandler
+
+class MyHandler(BaseHTTPRequestHandler):
+    def do_GET(self):
+        self.send_response(200)
+        self.send_header("Content-type", "text/plain")
+        self.end_headers()
+        self.wfile.write(b"Hello from Python server!")
+
+HOST = "0.0.0.0"
+PORT = 8000
+
+server = HTTPServer((HOST, PORT), MyHandler)
+
+print(f"Server running at http://{HOST}:{PORT}")
+server.serve_forever()
